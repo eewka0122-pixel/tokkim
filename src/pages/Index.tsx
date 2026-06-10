50481fd420cde0fd63017f39e69416095af72a5a
[dyad] <git@dyad.sh>
2026-06-10 21:12:31 +0300

    Updated src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import HeroSection from "@/components/HeroSection";
-import SignatureDishes from "@/components/SignatureDishes";
-import MenuSection from "@/components/MenuSection";
-import Gallery from "@/components/Gallery";
-import Reviews from "@/components/Reviews";
-import ReservationForm from "@/components/ReservationForm";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
-
 const Index = () => {
   return (
-    <main className="min-h-screen overflow-hidden bg-stone-950 text-stone-50">
-      <HeroSection />
-      <SignatureDishes />
-      <MenuSection />
-      <Gallery />
-      <Reviews />
-      <ReservationForm />
-      <ContactSection />
-      <Footer />
+    <main>
+      <h1>Hello</h1>
     </main>
   );
 };
